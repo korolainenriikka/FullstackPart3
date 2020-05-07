3a8c7dcbc02f005c64579a075ecdcd1c8b73db4e
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-05-07 18:59:11 +0300

    teht 11

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require('cors')
 
+const cors = require('cors')
 app.use(cors())
+
+app.use(express.static('build'))
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
