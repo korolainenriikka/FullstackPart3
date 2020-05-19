fb17cbaeed14f58da5155e87bb88fe2c9f0cbdc1
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-05-19 17:39:29 +0300

    heroku

---
+++ build/precache-manifest.53a487d1f9fd633b7e6518a297d70a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb120cebf98c17c6faf7fd67b25ea9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306f25d7266f99cabf46",
+    "url": "/static/css/main.998461a4.chunk.css"
+  },
+  {
+    "revision": "95dc697f1494aac99cc2",
+    "url": "/static/js/2.295ab629.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.295ab629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306f25d7266f99cabf46",
+    "url": "/static/js/main.362b5bd0.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);
